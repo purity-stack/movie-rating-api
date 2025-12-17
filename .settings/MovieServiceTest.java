b47d204f2ed1bc331d963ac88b81e87f0f5e9212
purity-stack <purityngure05@gmail.com>
2025-12-17 05:12:06 +0300

    Add files via upload

---
+++ .settings/MovieServiceTest.java
@@ -0,0 +1,40 @@
+package com.movierating.service;
+
+import com.movierating.Movie;
+import com.movierating.MovieRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class MovieServiceTest {
+
+    @Mock
+    private MovieRepository movieRepository;
+
+    @InjectMocks
+    private MovieService movieService;
+
+    @Test
+    void shouldReturnAllMovies() {
+
+        List<Movie> movies = List.of(
+                new Movie(1L, "Inception", 5),
+                new Movie(2L, "Matrix", 4)
+        );
+
+        when(movieRepository.findAll()).thenReturn(movies);
+
+        List<Movie> result = movieService.getAllMovies();
+
+        assertEquals(2, result.size());
+        assertEquals("Inception", result.get(0).getTitle());
+    }
+}
